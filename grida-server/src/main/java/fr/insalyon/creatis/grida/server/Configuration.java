0138ded00243e93451ab746515f6a5ea0a8ff8e1
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-04-15 10:53:27 +0200

    add illegal state exception when no config present

--- grida-server/src/main/java/fr/insalyon/creatis/grida/server/Configuration.java
+++ grida-server/src/main/java/fr/insalyon/creatis/grida/server/Configuration.java
@@ -147,45 +147,31 @@ public class Configuration {
     }
 
     private void loadConfigurationFile(File confFile) {
+        logger.info("Loading configuration file.");
 
         try {
-            logger.info("Loading configuration file.");
-            PropertiesConfiguration config = new PropertiesConfiguration(confFile);
+            if (confFile.exists()) {
+                PropertiesConfiguration config = new PropertiesConfiguration(confFile);
 
-            port = config.getInt(Constants.LAB_AGENT_PORT, 9006);
-            maxRetryCount = config.getInt(Constants.LAB_AGENT_RETRYCOUNT, 5);
-            minAvailableDiskSpace = config.getDouble(Constants.LAB_AGENT_MIN_AVAILABLE_DISKSPACE, 0.1);
-            preferredSEsList = config.getList(Constants.LAB_PREFERRED_SES, new ArrayList<String>());
-            cacheListMaxEntries = config.getInt(Constants.LAB_CACHE_MAX_ENTRIES, 30);
-            cacheListMaxHours = config.getInt(Constants.LAB_CACHE_MAX_HOURS, 12);
-            cacheFilesMaxSize = config.getDouble(Constants.LAB_CACHE_MAX_SIZE, 100) * 1024 * 1024;
-            cacheFilesPath = config.getString(Constants.LAB_CACHE_PATH, ".cache");
-            maxSimultaneousDownloads = config.getInt(Constants.LAB_POOL_MAX_DOWNLOAD, 10);
-            maxSimultaneousUploads = config.getInt(Constants.LAB_POOL_MAX_UPLOAD, 10);
-            maxSimultaneousDeletes = config.getInt(Constants.LAB_POOL_MAX_DELETE, 5);
-            maxSimultaneousReplications = config.getInt(Constants.LAB_POOL_MAX_REPLICATION, 5);
-            maxHistory = config.getInt(Constants.LAB_POOL_MAX_HISTORY, 120);
-
-            commandsType = config.getString(Constants.LAB_COMMANDS_TYPE, "dirac");
-            diracBashrc = config.getString(Constants.LAB_DIRAC_BASHRC, "needed_if_commands.type_is_dirac");
-
-            config.setProperty(Constants.LAB_AGENT_PORT, port);
-            config.setProperty(Constants.LAB_AGENT_RETRYCOUNT, maxRetryCount);
-            config.setProperty(Constants.LAB_AGENT_MIN_AVAILABLE_DISKSPACE, minAvailableDiskSpace);
-            config.setProperty(Constants.LAB_PREFERRED_SES, preferredSEsList);
-            config.setProperty(Constants.LAB_CACHE_MAX_ENTRIES, cacheListMaxEntries);
-            config.setProperty(Constants.LAB_CACHE_MAX_HOURS, cacheListMaxHours);
-            config.setProperty(Constants.LAB_CACHE_MAX_SIZE, cacheFilesMaxSize / (1024 * 1024));
-            config.setProperty(Constants.LAB_CACHE_PATH, cacheFilesPath);
-            config.setProperty(Constants.LAB_POOL_MAX_DOWNLOAD, maxSimultaneousDownloads);
-            config.setProperty(Constants.LAB_POOL_MAX_UPLOAD, maxSimultaneousUploads);
-            config.setProperty(Constants.LAB_POOL_MAX_DELETE, maxSimultaneousDeletes);
-            config.setProperty(Constants.LAB_POOL_MAX_REPLICATION, maxSimultaneousReplications);
-            config.setProperty(Constants.LAB_POOL_MAX_HISTORY, maxHistory);
-            config.setProperty(Constants.LAB_COMMANDS_TYPE, commandsType);
-            config.setProperty(Constants.LAB_DIRAC_BASHRC, diracBashrc);
-
-            config.save();
+                port = config.getInt(Constants.LAB_AGENT_PORT, 9006);
+                maxRetryCount = config.getInt(Constants.LAB_AGENT_RETRYCOUNT, 5);
+                minAvailableDiskSpace = config.getDouble(Constants.LAB_AGENT_MIN_AVAILABLE_DISKSPACE, 0.1);
+                preferredSEsList = config.getList(Constants.LAB_PREFERRED_SES, new ArrayList<String>());
+                cacheListMaxEntries = config.getInt(Constants.LAB_CACHE_MAX_ENTRIES, 30);
+                cacheListMaxHours = config.getInt(Constants.LAB_CACHE_MAX_HOURS, 12);
+                cacheFilesMaxSize = config.getDouble(Constants.LAB_CACHE_MAX_SIZE, 100) * 1024 * 1024;
+                cacheFilesPath = config.getString(Constants.LAB_CACHE_PATH, ".cache");
+                maxSimultaneousDownloads = config.getInt(Constants.LAB_POOL_MAX_DOWNLOAD, 10);
+                maxSimultaneousUploads = config.getInt(Constants.LAB_POOL_MAX_UPLOAD, 10);
+                maxSimultaneousDeletes = config.getInt(Constants.LAB_POOL_MAX_DELETE, 5);
+                maxSimultaneousReplications = config.getInt(Constants.LAB_POOL_MAX_REPLICATION, 5);
+                maxHistory = config.getInt(Constants.LAB_POOL_MAX_HISTORY, 120);
+    
+                commandsType = config.getString(Constants.LAB_COMMANDS_TYPE, "dirac");
+                diracBashrc = config.getString(Constants.LAB_DIRAC_BASHRC, "needed_if_commands.type_is_dirac");
+            } else {
+                throw new IllegalStateException("Configuration file must be present!");
+            }
 
         } catch (ConfigurationException ex) {
             logger.error(ex);
